0ca51e616beb4d137acdc83c43ac57b520a79cbd
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-07-20 23:15:15 +0100

    2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
 if number > 0:
-    print(number, 'is positive')
+    print(str(number) + " is positive",end="\n")
+elif number == 0:
+    print(str(number) + " is zero",end="\n")
 elif number < 0:
-        print(number, 'is negative')
-else: 
-        print(number, 'is zero')
+    print(str(number) + " is negative",end="\n")
