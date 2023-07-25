bb2c2f9efe5d700471dc4b0882f1fc505b67a492
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-07-25 19:02:28 +0100

    3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
     try:
-        result = a / a
+        result = a / b
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-    else:
+        result = None
+    finally:
         print("Inside result: {}".format(result))
         return result
-    finally:
-        print("Finally block executed.")
-        
-    # Test cases
-    print(safe_print_division(10, 2))
-    print(safe_print_division(10, 0))
-    print(safe_print_division(10, "2"))
