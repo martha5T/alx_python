3cf4589072d5b8147131000ebf2ad9a203b2b0a1
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-08-04 21:39:39 +0100

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
+
+
+def main():
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
+
+# Check if the script is being run directly
 if __name__ == "__main__":
-    print("{} + {} = {}".format(str(a), str(b), str(result)))
+    main()
