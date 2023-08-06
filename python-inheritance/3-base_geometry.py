80c67301162b4882ec030049e201d09a2dc4eca1
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-08-06 16:50:45 +0100

    updates

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,30 @@
-#!/usr/bin/python3
 """
-This is an empty class called base Geometry.
+Module: 3-base_geometry
+
+This module defines an empty base class for other geometry-related classes.
+
+Classes:
+    BaseGeometry: A base class for other geometry-related classes.
 """
-class BaseGeometry:
+
+class definitionOverrideMetaClass(type):
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+      
+
+
+class BaseGeometry(metaclass=definitionOverrideMetaClass):
     """
-    Empty class:
-        Base Geometry.
+    This class serves as the base for other geometry-related classes.
     """
     pass
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
