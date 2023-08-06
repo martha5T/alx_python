48a31067ba7ccd5cfe9d86ec01dfa88d3e2218ef
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-08-06 16:39:04 +0100

    updates

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -14,6 +14,7 @@ def inherits_from(obj, a_class):
     """
 
     return issubclass(type(obj), a_class) and type(obj) is not a_class
+   
     """
     Returns:
             True if the object is an instance of a_class; otherwise False.
