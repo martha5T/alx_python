1765509148187a0b71def0adc19f36bfc04158e9
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-08-06 16:54:33 +0100

    updates

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,17 +1,43 @@
-#!/usr/bin/python3
 """
-This function will messeage area() is not implemented.
+This module defines the BaseGeometry class.
+
+The BaseGeometry class is an abstract class that cannot be instantiated. It is used as a base class for other classes that will implement the area() method.
+
+The area() method is not implemented because the class BaseGeometry is an abstract class.
 """
-class BaseGeometry:
+
+
+class definitionOverrideMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BaseGeometry(definitionOverrideMetaClass('BaseGeometry', (), {})):
     """
-    This class represents a base geometry.
+    This class defines a public instance method called area() that raises an Exception exception with the message area() is not implemented.
     """
 
     def area(self):
         """
-        Calculates the area.
-
-        Raises:
-            Exception: with the message "area() is not implemented"
+        The area() method is not implemented because the class BaseGeometry is an abstract class. 
         """
+
         raise Exception("area() is not implemented")
+        """
+        Raises:
+        Exception: The message area() is not implemented
+        """
+    
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
