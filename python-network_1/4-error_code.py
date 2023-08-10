06381219eba976df0ccc8e1c8d734901d5f806e0
Martha Siiyabia Tong <125485604+Yabia5martha@users.noreply.github.com>
2023-08-10 20:52:38 +0100

    4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,31 +1,14 @@
-#!/usr/bin/python3
-"""
-This is a Python script that takes in a URL,
-sends a request to the URL and displays the body of the response.
-"""
 import requests
 import sys
 
-def fetch_response_body(url):
-    """
-    Fetches the body of the response from the given URL and prints it.
-
-    Args:
-        url (str): The URL to send the request to.
-
-    Raises:
-        requests.exceptions.RequestException: If an error occurs during the HTTP request.
-    """
+def get_request(url):
+    """Sends a GET request to the given URL."""
     response = requests.get(url)
-
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-    else:
-        print(response.text)
+        print("Error code:", response.status_code)
+    return response.text
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        fetch_response_body(url)
-    except requests.exceptions.RequestException as e:
-        print("Error:", str(e))
+    response_text = get_request(url)
+    print(response_text)
