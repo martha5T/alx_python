78ff0a4f336544d51324841c95946d244ea297ab
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-09-07 21:10:17 -0700

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,37 +1,41 @@
-"""takes in the name of a state as an argument and lists all cities"""
+#!/usr/bin/python3
+
+"""
+ takes in the name of a state as an argument and lists all cities of that
+ state, using the database hbtn_0e_4_usa
+"""
 
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state = sys.argv[4]
+    """connect to sql server"""
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host="localhost",
+                         port=3306)
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    """create a cursor object"""
+    cursor = db.cursor()
 
-    cur = db.cursor()
+    """query"""
+    sql = """SELECT cities.name
+        FROM states
+        INNER JOIN cities ON states.id=cities.state_id
+        WHERE states.name = %s
+        ORDER by cities.id ASC"""
 
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
+    """execute the query"""
+    cursor.execute(sql, (sys.argv[4], ))
 
-    cur.execute(query, (state,))
+    """fetch data"""
+    data = cursor.fetchall()
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    """print data"""
+    print(", ".join([city[0] for city in data]))
 
-    cur.close()
+    """close cursor and connection"""
+    cursor.close()
     db.close()
