687530f732330f078f97d4b1712f296cf2a94e99
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-10-28 21:56:49 -0700

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
